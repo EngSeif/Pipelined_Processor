7a5b73be9a04ef5373ccdd893b1485385c07d158
engseif <eng.seifmohamed2004@gmail.com>
2025-05-09 18:36:37 +0300

    Update Control Unit

--- Control_Register/Control_Unit.vhdl
+++ Control_Register/Control_Unit.vhdl
@@ -22,10 +22,10 @@ ENTITY Control_unit IS
 
         -- outputs
         wb_ctrl : output STD_LOGIC_VECTOR(2 DOWNTO 0);
-        pc_src : output STD_LOGIC_VECTOR(2 DOWNTO 0);
+        pc_src : output STD_LOGIC_VECTOR(1 DOWNTO 0);
+        address_sel : output STD_LOGIC_VECTOR(1 DOWNTO 0);
         mem_read : output STD_LOGIC;
         mem_write : output STD_LOGIC;
-        address_sel : output STD_LOGIC;
         sp_up_down : output STD_LOGIC;
         stall_flag : output STD_LOGIC;
         hlt_flag : output STD_LOGIC;
@@ -34,6 +34,13 @@ ENTITY Control_unit IS
     );
 
 END ENTITY Control_unit;
+
+-- pc_src       ✅
+-- mem_read     ✅
+-- mem_write    ✅
+-- Address_sel  ✅
+-- hlt_flag     ✅
+-- reg_write_2  ✅
 
 ARCHITECTURE control_arch OF Control_unit IS
 
@@ -55,6 +62,90 @@ BEGIN
             pc_src <= "11";
         ELSE
             pc_src <= "00"; -- Normal execution (PC + 1)
+        END IF;
+    END PROCESS;
+
+    -- MemRead Handling Logic
+    PROCESS (instruction_bits)
+    BEGIN
+        IF (instruction_bits(31 DOWNTO 26) = "100010") THEN -- LDD (LOAD FROM MEMORY)
+            mem_read <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "011010") THEN -- POP
+            mem_read <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000011") THEN -- RET
+            mem_read <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000101") THEN -- RTI
+            mem_read <= "1";
+        ELSE
+            mem_read <= "0"; -- Normal execution (Command Doesnt Need Memory Read)
+        END IF;
+    END PROCESS;
+
+    -- MemWrite/wb_ctrl Handling Logic
+    PROCESS (instruction_bits)
+    BEGIN
+        IF (instruction_bits(31 DOWNTO 26) = "100011") THEN -- STD (Store In MEMORY)
+            mem_write <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "011001") THEN -- PUSH
+            mem_write <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "100100") THEN -- CALL
+            mem_write <= "1";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000100") THEN -- INT
+            mem_write <= "1";
+        ELSIF (Int = '1') THEN -- INT EXTERNAL
+            mem_write <= "1";
+        ELSE
+            mem_write <= "0"; -- Normal execution (Command Doesnt Need Memory Write)
+        END IF;
+    END PROCESS;
+
+    -- address_sel Handling Logic
+    -- 1st => SP Selection
+    -- 2nd => Load And Store
+    -- 3rd => Interrupts
+    PROCESS (instruction_bits)
+    BEGIN
+        IF (instruction_bits(31 DOWNTO 26) = "011001") THEN -- PUSH
+            address_sel <= "01";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "011010") THEN -- POP
+            address_sel <= "01";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "100100") THEN -- CALL
+            address_sel <= "01";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000011") THEN -- RET
+            address_sel <= "01";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000101") THEN -- RTI
+            address_sel <= "01";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "100010") THEN -- LDD (LOAD FROM MEMORY)
+            address_sel <= "10";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "100011") THEN -- STD (Store In MEMORY)
+            address_sel <= "10";
+        ELSIF (instruction_bits(31 DOWNTO 26) = "000100") THEN -- INT
+            address_sel <= "11";
+        ELSIF (Int = '1') THEN -- INT EXTERNAL
+            address_sel <= "11";
+        ELSE
+            address_sel <= "00"; -- Normal execution (Take PC)
+        END IF;
+    END PROCESS;
+
+    -- handle hlt flag
+    PROCESS (instruction_bits)
+    BEGIN
+        IF (instruction_bits(31 DOWNTO 26) = "000001") THEN -- HLT
+            hlt_flag <= "1";
+        ELSE
+            hlt_flag <= "0"; -- Normal 
+        END IF;
+    END PROCESS;
+
+    PROCESS (instruction_bits)
+    BEGIN
+        IF (instruction_bits(31 DOWNTO 26) = "010101") THEN -- SWAP
+            reg_write_2 <= "1";
+            wb_ctrl <= "1";
+        ELSE
+            reg_write_2 <= "0"; -- Normal 
+            wb_ctrl <= "0";
         END IF;
     END PROCESS;
 
